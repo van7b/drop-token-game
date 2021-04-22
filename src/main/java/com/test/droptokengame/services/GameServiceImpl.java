22c360359b78c4c9200a9bbba99ebb0ca9a0a732
Vandana Baidya <22742523+van7b@users.noreply.github.com>
2021-04-22 21:14:39 +0530

    updated postAMove method

--- src/main/java/com/test/droptokengame/services/GameServiceImpl.java
+++ src/main/java/com/test/droptokengame/services/GameServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.test.droptokengame.exceptions.GameCollectionException;
 import com.test.droptokengame.models.DeleteResponse;
 import com.test.droptokengame.models.Game;
@@ -73,33 +74,48 @@ public class GameServiceImpl implements GameService {
 	public Move postAMove(Move move, Game game, String gameId, String playerId)
 		throws GameCollectionException {
 			Optional<Game> gameOptional = gameRepository.findByGameId(gameId);
-			if(gameOptional.isPresent()) {
+			if(gameOptional.isPresent() && gameOptional.get().getState().equals(State.IN_PROGRESS)) {
 				Optional<String> player = gameRepository.findByPlayerId(playerId);
 				if(player.isPresent()) {
-					move.setPlayerId(playerId);
-					move.setColumn(move.getColumn());
-					
-					Integer column = move.getColumn();
-					if(column <= 0 || column > gameOptional.get().getNoOfColumns()) {
-						throw new GameCollectionException(GameCollectionException.ColumnOutOfBoundException(column));
-					} else {
-						game = gameOptional.get();
-						game.getMoves().add(move);
-						String board[][] = game.getBoard();
-						int i;
-						for(i=game.getNoOfRows()-1;i>=0;i--) {
-							if(board[i][column-1] == null) {
-								board[i][column-1] = playerId;
-								game.setBoard(board);
-								break;
-							}
-						}	
-						if(i<0) {
-							throw new GameCollectionException(GameCollectionException.ColumnAlreadyFilledException(column));
+					if((gameOptional.get().getLastPlayer()==null) || (!gameOptional.get().getLastPlayer().equals(playerId))) {
+						move.setPlayerId(playerId);
+						move.setColumn(move.getColumn());
+						
+						Integer column = move.getColumn();
+						if(column <= 0 || column > gameOptional.get().getNoOfColumns()) {
+							throw new GameCollectionException(GameCollectionException.ColumnOutOfBoundException(column));
+						} else {
+								game = gameOptional.get();
+								game.getMoves().add(move);
+								game.setLastPlayer(playerId);
+								
+								String board[][] = game.getBoard();
+								int i;
+								for(i=game.getNoOfRows()-1;i>=0;i--) {
+									if(board[i][column-1] == null) {
+										board[i][column-1] = playerId;
+										game.setBoard(board);
+										break;
+									}
+								}	
+								if(i<0) {
+									throw new GameCollectionException(GameCollectionException.ColumnAlreadyFilledException(column));
+								}
+								
+								
+								if(checkWinner(game)) {
+									game.setState(State.DONE);
+									game.setWinner(playerId);
+								}
+								gameRepository.save(game);
+								return move;
 						}
-						gameRepository.save(game);
-						return move;
+						
+						
+					}else {
+						throw new GameCollectionException(GameCollectionException.InvalidPlayerTurnException(playerId));
 					}
+					
 				} else {
 					throw new GameCollectionException(GameCollectionException.PlayerNotFoundException(playerId));
 				}
@@ -148,108 +164,70 @@ public class GameServiceImpl implements GameService {
 		}
 		
 	}
-
-	@Override
-	public void playGame(Game game, String gameId) throws GameCollectionException {
-		Optional<Game> gameOptional = gameRepository.findByGameId(gameId);
-		if(gameOptional.isPresent()) {
-			game = gameOptional.get();
-			if(game.getState().equals(State.DONE)) {
-				throw new GameCollectionException(GameCollectionException.GameNotInProgressException(gameId));
-			}
-			
-			Move move = new Move();
-			int count = 0;
-			while(true) {
-//				move = (count%2 == 0) ? postAMove(new Move(), game, gameId, game.getPlayer1()) 
-//						: postAMove(new Move(), game, gameId, game.getPlayer2());
-				
-				if(count%2 == 0) {
-					System.out.println("Enter a column number by Player1 - "+game.getPlayer1()+": ");
-					move = postAMove(new Move(), game, gameId, game.getPlayer1());
-				} else {
-					System.out.println("Enter a column number by Player2 - "+game.getPlayer2()+": ");
-					move = postAMove(new Move(), game, gameId, game.getPlayer2());
-				}
-				
-				count++;
-				gameRepository.save(game);
-				checkWinner(game);
-				
-				if(game.getState().equals(State.DONE)) {
-					break;
-				} else if(count == game.getNoOfRows()*game.getNoOfColumns() && game.getState().equals(State.IN_PROGRESS)) {
-					game.setState(State.DONE);
-					break;
-				}
-			}
-		}
-	}
 	
-	private void checkWinner(Game game) {
+	private boolean checkWinner(Game game) {
 		String board[][] = game.getBoard();
 		
 		int i, j;
 		//check row wise
 		for(i=0;i<game.getNoOfRows();i++) {
-			String player = board[i][0];
-			for(j=1;j<game.getNoOfColumns();j++) {
-				if(board[i][j] == null || !board[i][j].equals(player)) {
-					break;
+			if(board[i][0] != null) {
+				String player = board[i][0];
+				for(j=1;j<game.getNoOfColumns();j++) {
+					if(board[i][j] == null || !board[i][j].equals(player)) {
+						break;
+					}
 				}
-			}
-			if(j==game.getNoOfColumns()) {
-				game.setState(State.DONE);
-				game.setWinner(player);
-				gameRepository.save(game);
-				return;
+				if(j==game.getNoOfColumns()) {
+					return true;
+				}
 			}
 		}
 		
 		
 		//check column wise
 		for(j=0;j<game.getNoOfColumns();j++) {
-			String player = board[0][j];
-			for(i=1;i<game.getNoOfRows();i++) {
-				if(board[i][j] == null || board[i][j].equals(player)) {
-					break;
+			if(board[0][j] != null) {
+				String player = board[0][j];
+				for(i=1;i<game.getNoOfRows();i++) {
+					if(board[i][j] == null || !board[i][j].equals(player)) {
+						break;
+					}
 				}
-			}
-			if(i==game.getNoOfRows()) {
-				game.setState(State.DONE);
-				game.setWinner(player);
-				gameRepository.save(game);
-				return;
+				if(i==game.getNoOfRows()) {
+					return true;
+				}
 			}
 		}
 		
 		//check diagonal
-		String player = board[0][0];
-		for(i=1;i<game.getNoOfRows();i++) {
-			if(!board[i][i].equals(player)) {
-				break;
+		String player = "";
+		if(board[0][0] != null) {
+			player = board[0][0];
+			for(i=1;i<game.getNoOfRows();i++) {
+				if(!board[i][i].equals(player)) {
+					break;
+				}
 			}
-		}
-		if(i==game.getNoOfRows()) {
-			game.setState(State.DONE);
-			game.setWinner(player);
-			gameRepository.save(game);
-			return;
+			if(i==game.getNoOfRows()) {
+				return true;
+			}
 		}
 		
 		//check diagonal(top right to bottom left)
-		player = board[0][game.getNoOfColumns()-1];
-		for(i=1;i<game.getNoOfRows();i++) {
-			if(!board[game.getNoOfRows()-i-1][i].equals(player)) {
-				break;
+		if(board[0][game.getNoOfColumns()-1] != null) {
+			player = board[0][game.getNoOfColumns()-1];
+			for(i=1;i<game.getNoOfRows();i++) {
+				if(!board[game.getNoOfRows()-i-1][i].equals(player)) {
+					break;
+				}
+			}
+			if(i==game.getNoOfRows()) {
+				return true;
 			}
 		}
-		if(i==game.getNoOfRows()) {
-			game.setState(State.DONE);
-			game.setWinner(player);
-			gameRepository.save(game);
-			return;
-		}
+		
+		return false;
 	}
 	
 }

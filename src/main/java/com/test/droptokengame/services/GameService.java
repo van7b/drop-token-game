49b2ee930c1ddfc8e326830a7bbe3bf6bbf245ec
Vandana Baidya <22742523+van7b@users.noreply.github.com>
2021-04-25 16:39:08 +0530

    Replaced GameCollectionException

--- src/main/java/com/test/droptokengame/services/GameService.java
+++ src/main/java/com/test/droptokengame/services/GameService.java
@@ -2,7 +2,14 @@ package com.test.droptokengame.services;
 
 import java.util.List;
 
-import com.test.droptokengame.exceptions.GameCollectionException;
+import com.test.droptokengame.exceptions.ColumnAlreadyFilledException;
+import com.test.droptokengame.exceptions.ColumnOutOfBoundException;
+import com.test.droptokengame.exceptions.GameNotFoundException;
+import com.test.droptokengame.exceptions.GameNotInProgressException;
+import com.test.droptokengame.exceptions.InvalidMoveNumberException;
+import com.test.droptokengame.exceptions.InvalidPlayerTurnException;
+import com.test.droptokengame.exceptions.PlayerNotFoundException;
+import com.test.droptokengame.exceptions.SamePlayersException;
 import com.test.droptokengame.models.DeleteResponse;
 import com.test.droptokengame.models.Game;
 import com.test.droptokengame.models.GetStateOfGameResponse;
@@ -11,16 +18,17 @@ import com.test.droptokengame.models.MoveResponse;
 
 public interface GameService {
 
-	public void createNewGame(Game game) throws GameCollectionException;
+	public void createNewGame(Game game) throws SamePlayersException;
 
 	public List<String> getAllGamesInProgress();
 
-	public GetStateOfGameResponse getStateOfTheGame(String gameId) throws GameCollectionException;
+	public GetStateOfGameResponse getStateOfTheGame(String gameId) throws GameNotFoundException;
 
-	public Move postAMove(Move move, Game game, String gameId, String playerId) throws GameCollectionException;
+	public Move postAMove(Move move, String gameId, String playerId) throws ColumnOutOfBoundException, 
+			ColumnAlreadyFilledException, InvalidPlayerTurnException, PlayerNotFoundException, GameNotFoundException;
 	
-	public MoveResponse getMoveByMoveNumber(Game game, String gameId, int moveNumber) throws GameCollectionException;
+	public MoveResponse getMoveByMoveNumber(Game game, String gameId, int moveNumber) throws InvalidMoveNumberException, GameNotFoundException;
 
-	public DeleteResponse deletePlayerId(Game game, String gameId, String playerId) throws GameCollectionException;
+	public DeleteResponse deletePlayerId(Game game, String gameId, String playerId) throws GameNotInProgressException, PlayerNotFoundException, GameNotFoundException;
 	
 }

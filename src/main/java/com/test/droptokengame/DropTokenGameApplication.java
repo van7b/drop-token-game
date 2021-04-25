90df8caca0e4d49bb5ff2522c817b4958e5c795e
Vandana Baidya <22742523+van7b@users.noreply.github.com>
2021-04-25 16:43:00 +0530

    Added whiteLabelError removal annotation

--- src/main/java/com/test/droptokengame/DropTokenGameApplication.java
+++ src/main/java/com/test/droptokengame/DropTokenGameApplication.java
@@ -2,8 +2,9 @@ package com.test.droptokengame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class DropTokenGameApplication {
 
 	public static void main(String[] args) {
